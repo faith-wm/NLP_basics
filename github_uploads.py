b64698da99187937a0ca4f6863b6d7f24cffb6c0
Faith Wavinya <45032405+faith-wm@users.noreply.github.com>
2020-03-18 11:11:50 +0900

    Add files via upload

---
+++ github_uploads.py
@@ -0,0 +1,23 @@
+
+
+#pos tagging with nltk
+import nltk
+sentence='the patient has a headache'
+tokens=nltk.word_tokenize(sentence)
+pos_tagged_text=nltk.pos_tag(tokens)
+print(pos_tagged_text)
+
+
+
+#pos tagging with spacy
+import spacy
+nlp=spacy.load('en')
+sentence=nlp(sentence)
+
+pos_tagged_text=[(i, i.tag_) for i in sentence]
+print(pos_tagged_text)
+
+
+
+
+
